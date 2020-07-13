839793df7f2a44a7efb2dadec4a9a4945b56cb2b
christopherdahm <christopher.dahm@semanticbits.com>
2020-07-13 11:17:26 -0400

    First custom action test

---
+++ .github/actions/forked-cache/src/constants.ts
@@ -0,0 +1,23 @@
+export enum Inputs {
+  Key = "key",
+  Path = "path",
+  RestoreKeys = "restore-keys",
+  RestoreOnly = "restore-only",
+}
+
+export enum Outputs {
+  CacheHit = "cache-hit",
+}
+
+export enum State {
+  CachePrimaryKey = "CACHE_KEY",
+  CacheMatchedKey = "CACHE_RESULT",
+}
+
+export enum Events {
+  Key = "GITHUB_EVENT_NAME",
+  Push = "push",
+  PullRequest = "pull_request",
+}
+
+export const RefKey = "GITHUB_REF";
---
+++ .github/actions/forked-cache/src/save.ts
@@ -0,0 +1,63 @@
+import * as cache from "@actions/cache";
+import * as core from "@actions/core";
+
+import { Events, Inputs, State } from "./constants";
+import * as utils from "./utils/actionUtils";
+
+async function run(): Promise<void> {
+  try {
+    if (!utils.isValidEvent()) {
+      utils.logWarning(
+        `Event Validation Error: The event type ${
+          process.env[Events.Key]
+        } is not supported because it's not tied to a branch or tag ref.`
+      );
+      return;
+    }
+
+    if (core.getInput(Inputs.RestoreOnly) === "true") {
+      core.info(
+        "Cache action configured for restore-only, skipping save step."
+      );
+      return;
+    }
+
+    const state = utils.getCacheState();
+
+    // Inputs are re-evaluted before the post action, so we want the original key used for restore
+    const primaryKey = core.getState(State.CachePrimaryKey);
+    if (!primaryKey) {
+      utils.logWarning(`Error retrieving key from state.`);
+      return;
+    }
+
+    if (utils.isExactKeyMatch(primaryKey, state)) {
+      core.info(
+        `Cache hit occurred on the primary key ${primaryKey}, not saving cache.`
+      );
+      return;
+    }
+
+    const cachePaths = utils.getInputAsArray(Inputs.Path, {
+      required: true
+    });
+
+    try {
+      await cache.saveCache(cachePaths, primaryKey);
+    } catch (error) {
+      if (error.name === cache.ValidationError.name) {
+        throw error;
+      } else if (error.name === cache.ReserveCacheError.name) {
+        core.info(error.message);
+      } else {
+        utils.logWarning(error.message);
+      }
+    }
+  } catch (error) {
+    utils.logWarning(error.message);
+  }
+}
+
+run();
+
+export default run;
